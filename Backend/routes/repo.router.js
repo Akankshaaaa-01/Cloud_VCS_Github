0e675c4067e7f9f8c6109763f9568da1c96cb9a7
Akanksha <akankksha75@gmail.com>
2026-04-21 19:46:55 +0530

    Added file controller to fetch files from S3 and updated repo routes

--- Backend/routes/repo.router.js
+++ Backend/routes/repo.router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const repoController = require("../controllers/repoController");
+const { getRepoFiles, getFileContent } = require("../controllers/fileController");
 const { authenticateMiddleware } = require("../middleware/authenticateMiddleware");
 
 const authorizeMiddleware = require("../middleware/authorizeMiddleware");
@@ -11,6 +12,8 @@ repoRouter.get("/repo/all", repoController.getAllRepositories);
 repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
 repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
 repoRouter.get("/repo/user/:userID", repoController.fetchRepositoriesForCurrentUser);
+repoRouter.get("/repo/:id/files", getRepoFiles);
+repoRouter.get("/repo/:id/file", getFileContent);
 
 // Sirf logged-in user kar sakta hai — authenticate only
 repoRouter.post("/repo/create", authenticateMiddleware, repoController.createRepository);
